7b9f3e4badafac251ef73866a0b1754445019ff9
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2018-01-11 12:59:24 -0700

    Array List example

---
+++ src/com/company/ScottsArrayList.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScottsArrayList {
+    public static void main(String[] args) {
+        List<String> scottsList = new ArrayList<>();
+        scottsList.add("Cats");
+        scottsList.add("Dogs");
+        scottsList.add("Hermit Crab");
+        scottsList.add("Mouse");
+        scottsList.add("Turtle");
+
+        System.out.println(scottsList);
+
+    }
+}
